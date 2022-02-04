db8663a336f706ee9980ef88feb42b63bb755bc1
pk1411 <pulkit.kejriwal@crio-users.in>
2022-02-04 16:50:12 +0530

    first commit

---
+++ secret.js
@@ -0,0 +1,3 @@
+ const contractAddress = '0x6f40d4a6237c257fff2db00fa0510deeecd303eb';
+ const infuraLink = 'https://mainnet.infura.io/v3/136babc1becc456c948b63e90186871e';
+ module.exports = { contractAddress ,infuraLink }
